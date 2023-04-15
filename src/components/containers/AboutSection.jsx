9d8f33c8ac556cffc1af6d4f19e0bf24e5be4aad
Meeet <meetbhure286@gmail.com>
2023-04-15 17:30:28 +0530

    details changes

--- src/components/containers/AboutSection.jsx
+++ src/components/containers/AboutSection.jsx
@@ -112,7 +112,7 @@ const AboutSection = () => {
               </li>
             )}
           </ul>
-          <a href="/MeetResume.pdf" className="btn mt-3">
+          <a href="/Meet's_CV.pdf" className="btn mt-3">
             <span>Download Resume</span>
           </a>
         </motion.div>
